6103ba5b298d6d3ca2f4f7836c599b649030d5dd
JACK_1729 <jack17529@users.noreply.github.com>
2017-06-29 20:15:04 +0530

    Add files via upload

---
+++ WebSite15/TeamB.js
@@ -0,0 +1,13 @@
+﻿var pragimTech = pragimTech || {};
+pragimTech.TeamB = pragimTech.TeamB || {};
+
+pragimTech.TeamB.customer = function(firstName, middleName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleName = middleName;
+
+    this.getFullName = function () {
+        return this.firstName + " " + this.middleName + " " + this.lastName;
+    }
+    return this;
+}
